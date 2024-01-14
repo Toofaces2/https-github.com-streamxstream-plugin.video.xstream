b0fba869c3d312912008381128c850d1dfaab1ea
DWH-WFC <bunny-jaeger@web.de>
2024-01-14 17:33:36 +0100

    einfach

--- sites/einfach.py
+++ sites/einfach.py
@@ -217,11 +217,10 @@ def showHosters():
     if isMatch:
         isMatch, aResult = cParser.parse(sHtmlContainer, 'data-href="(h[^"]+).*?>([^<]+)')  # sUrl + sName
     if not isMatch: return
-    sQuality = '720'
     for sUrl, sName in aResult:
         if 'Trailer' in sName: continue  # Trailer ausblenden
-        #if cConfig().isBlockedHoster(sName)[1]: continue  # Hoster aus settings.xml oder deaktivierten Resolver ausschließen
-        hoster = {'link': sUrl, 'name': sName, 'displayedName': '%s [I][%sp][/I]' % (sName, sQuality), 'quality': sQuality}
+        #if cConfig().isBlockedHoster(sName)[0]: continue # Hoster aus settings.xml oder deaktivierten Resolver ausschließen
+        hoster = {'link': sUrl, 'name': sName }
         hosters.append(hoster)
     if hosters:
         hosters.append('getHosterUrl')
